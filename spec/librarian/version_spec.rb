f2197665d3b78d9bcf932673baf0cf05b2a7a7da
akihiro uesaka <akihiro.uesaka@gmail.com>
2017-01-22 12:16:11 +0900

    move version spec

---
+++ spec/librarian/version_spec.rb
@@ -0,0 +1,7 @@
+require "spec_helper"
+
+describe Librarian do
+  it "has a version number" do
+    expect(Librarian::VERSION).not_to be nil
+  end
+end
--- spec/librarian_spec.rb
+++ spec/librarian_spec.rb
@@ -1,7 +1,4 @@
 require "spec_helper"
 
 describe Librarian do
-  it "has a version number" do
-    expect(Librarian::VERSION).not_to be nil
-  end
 end
